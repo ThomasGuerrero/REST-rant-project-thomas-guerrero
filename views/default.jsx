0070b2263f89cad4a099022ba8017afc4bb8c24d
Thomas Guerrero <thomaseguerrero@gmail.com>
2023-05-09 13:45:42 -0400

    Adding styles to REST-rant

--- views/default.jsx
+++ views/default.jsx
@@ -4,6 +4,8 @@ function Default (html) {
         <html>
             <head>
                 <title>Title</title>
+                <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css'></link>
+                <link rel='stylesheet' href='/css/style.css'></link>
             </head>
             <body>
                 {html.children}
